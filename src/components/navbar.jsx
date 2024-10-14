d6da6f2d79f3690082167c14bc48284774c758b8
royeshmalvankar <royeshmalvankar@gmail.com>
2024-10-14 19:45:31 +0530

    cwproject

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import profile from '../assets/profile.png'
-import fav from '../assets/fav.png'
+// import fav from '../assets/fav.png'
 import shoppingbag from '../assets/shoppingbag.png'
 import search from '../assets/search.png'
 import { AuthContext } from '../authcontext/authcontext'
